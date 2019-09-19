774d43ddafb785d55a7b34c1f96771a2946b63e7
Pierrick Bignet <pierrick.bignet@gmail.com>
2019-09-19 17:36:05 +0200

    Fix webpack deprecation warning

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const dotenv = require('dotenv-extended');
-const fs = require('fs');
-const EnvironmentPlugin = require('webpack').EnvironmentPlugin;
+const dotenv = require("dotenv-extended");
+const EnvironmentPlugin = require("webpack").EnvironmentPlugin;
 
 module.exports = DotenvPlugin;
 
@@ -8,7 +7,7 @@ function DotenvPlugin(options) {
   this.options = options || {};
 
   // Load into process.env, and keep track of all the
-  // keys we care about for webpack serialization. 
+  // keys we care about for webpack serialization.
   this.config = dotenv.load(options) || {};
 }
 
@@ -16,14 +15,17 @@ DotenvPlugin.prototype.apply = function(compiler) {
   if (this.options.verbose) {
     const definitions = {};
 
-    Object.keys(this.config).forEach((key) => {
+    Object.keys(this.config).forEach(key => {
       if (this.config.hasOwnProperty(key)) {
         definitions[key] = process.env[key];
       }
     });
 
-    console.log('Applying dotenv configuration', JSON.stringify(definitions, null, 2));
+    console.log(
+      "Applying dotenv configuration",
+      JSON.stringify(definitions, null, 2)
+    );
   }
 
-  compiler.apply(new EnvironmentPlugin(Object.keys(this.config)));
+  new EnvironmentPlugin(Object.keys(this.config)).apply(compiler);
 };
